ddf775d67cb7f055333b8d8ea7fa0c6197938ad5
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-24 06:59:45 +0100

    commit zero

---
+++ Adopt me Script pastebin roblox Adopt Me! GUI .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://pastebin.com/raw/Z3iAXASr", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Adopt me the script.It has teleports, Setspeed, and more!
+						
+]]
