8f85ece8c1d9bf4daf2cb24733eb312ca1e72ead
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:20:15 +0100

    commit zero

---
+++ Adopt Me Script pastebin roblox Adopt Me .lua
@@ -0,0 +1,12 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://pastebin.com/raw/J5DR2Xk0", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Super cool script for the most popular game in the unlocked
+Adopt me
+						
+]]
